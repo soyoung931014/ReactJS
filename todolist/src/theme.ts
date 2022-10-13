213ec344070925ebd1b6c828e831a4f4709ee12d
park_so_young <soyoung931014@gmail.com>
2022-10-13 17:04:29 +0900

    chore: todolist initialize

---
+++ todolist/src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from 'styled-components';
+
+export const darkTheme: DefaultTheme = {
+  bgColor: '#2f3640',
+  textColor: '#f5f6fa',
+  accentColor: '#4cd137',
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: 'white',
+  textColor: '#f5f6fa',
+  accentColor: '#4cd137',
+};
