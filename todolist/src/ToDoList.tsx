6408b958b079c9e74cf8923ecc92bc53635bf531
park_so_young <soyoung931014@gmail.com>
2022-10-13 17:55:33 +0900

    feat: 리액트 훅 폼 사용전코드

--- todolist/src/ToDoList.tsx
+++ todolist/src/ToDoList.tsx
@@ -2,21 +2,27 @@ import React, { useState } from "react";
 
 const ToDoList = () => {
   const [todo, setTodo] = useState("");
+  const [toDoError, setToDoError] = useState("");
   const changeTodo = (e: React.FormEvent<HTMLInputElement>) => {
     const {
       currentTarget: { value },
     } = e;
+    setToDoError("");
     setTodo(value);
   };
   const submit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("todo:", todo);
+    if (todo.length < 10) {
+      return setToDoError("to do should be longer");
+    }
+    console.log("submit");
   };
   return (
     <div>
       <form onSubmit={submit}>
         <input placeholder="writing todo" onChange={changeTodo} value={todo} />
         <button>click</button>
+        <div>{toDoError !== "" ? toDoError : null}</div>
       </form>
     </div>
   );
