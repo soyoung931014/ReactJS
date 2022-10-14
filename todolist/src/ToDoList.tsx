0db91aed592805e9417f63db82724c67b728c14d
park_so_young <soyoung931014@gmail.com>
2022-10-14 20:50:16 +0900

    chore: form Validation, errors

--- todolist/src/ToDoList.tsx
+++ todolist/src/ToDoList.tsx
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 const ToDoList = () => {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  console.log(handleSubmit);
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
   return (
     <div>
-      <form>
-        <input placeholder="email" {...register("email")} />
-        <input placeholder="password" {...register("password")} />
-        <input placeholder="frstName" {...register("frstName")} />
-        <input placeholder="secondName" {...register("secondName")} />
-        <input placeholder="thirdName" {...register("thirdName")} />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input placeholder="email" {...register("email", { required: true })} />
+        <input
+          placeholder="password"
+          {...register("password", { required: true, minLength: 10 })}
+        />
+        <input
+          placeholder="frstName"
+          {...register("frstName", { required: true })}
+        />
+        <input
+          placeholder="secondName"
+          {...register("secondName", { required: true })}
+        />
+        <input
+          placeholder="thirdName"
+          {...register("thirdName", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
+        />
         <button>click</button>
       </form>
     </div>
