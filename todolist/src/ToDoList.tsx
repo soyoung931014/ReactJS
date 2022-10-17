79cd7efb3da39a0fe8207970f7a4ed263c0c62d3
park_so_young <soyoung931014@gmail.com>
2022-10-17 16:49:50 +0900

    feat: validaton

--- todolist/src/ToDoList.tsx
+++ todolist/src/ToDoList.tsx
@@ -1,30 +1,92 @@
 import { useForm } from "react-hook-form";
+interface IForm {
+  [key: string]: string;
+}
+
+// interface IForm {
+//   email: string;
+//   firstName: string;
+//   lastName: string;
+//   username: string;
+//   password: string;
+//   password1: string;
+// }
+
 const ToDoList = () => {
-  const { register, handleSubmit, formState } = useForm();
-  console.log(handleSubmit);
-  const onValid = (data: any) => {
-    console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "naver.com",
+    },
+  });
+  const onValid = (data: IForm) => {
+    console.log(data); // {email: 'so@naver.com', password: '33333d', passwordCheck: '33333333', secondName: '33', thirdName: '333333'}
+    if (data.password !== data.passwordCheck) {
+      setError(
+        "passwordCheck",
+        { message: "password are not same" },
+        { shouldFocus: true }
+      );
+    }
+    //  setError("extraError", { message: "Server offline" });
   };
-  console.log(formState.errors);
+
   return (
     <div>
       <form
         style={{ display: "flex", flexDirection: "column" }}
         onSubmit={handleSubmit(onValid)}
       >
-        <input placeholder="email" {...register("email", { required: true })} />
+        <input
+          placeholder="email"
+          {...register("email", {
+            required: "email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "only naver.com emails allowed",
+            },
+          })}
+        />
+        <span style={{ color: "red" }}>{errors?.email?.message}</span>
         <input
           placeholder="password"
-          {...register("password", { required: true, minLength: 10 })}
+          {...register("password", {
+            required: "password is required",
+            minLength: {
+              value: 5,
+              message: "your password is too short",
+            },
+          })}
         />
+        <span style={{ color: "red" }}>{errors?.password?.message}</span>
         <input
-          placeholder="frstName"
-          {...register("frstName", { required: true })}
+          placeholder="password check"
+          {...register("passwordCheck", {
+            required: "passwordCheck is required",
+            minLength: {
+              value: 5,
+              message: "your passwordCheck is too short",
+            },
+          })}
         />
+        <span style={{ color: "red" }}>{errors?.passwordCheck?.message}</span>
         <input
-          placeholder="secondName"
-          {...register("secondName", { required: true })}
+          placeholder="firstName"
+          {...register("firstName", {
+            required: "write here",
+            validate: {
+              nosoyoung: (value) =>
+                value.includes("soyoung") ? "soyoung is not allowed" : true,
+              noNico: (value) =>
+                value.includes("nico") ? "nico is not allowed" : true,
+            }, // value가 soyoung을 포함하지 않는다면 true를 반환할것이다. false면 focus진행됨
+          })}
         />
+        <span style={{ color: "red" }}>{errors?.firstName?.message}</span>
         <input
           placeholder="thirdName"
           {...register("thirdName", {
