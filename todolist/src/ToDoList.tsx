8bccd3098f2b3b77a361baeccc281d263b2a0972
park_so_young <soyoung931014@gmail.com>
2022-10-20 01:02:02 +0900

    feat: selector part one

--- todolist/src/ToDoList.tsx
+++ todolist/src/ToDoList.tsx
@@ -1,17 +1,31 @@
-import { toDoState } from "atom";
+import { toDoSelector, toDoState } from "atom";
 import CreateToDo from "CreateToDo";
 import { useRecoilValue } from "recoil";
 import ToDo from "ToDo";
 
 const ToDoList = () => {
   const toDos = useRecoilValue(toDoState);
-  console.log(toDos);
+  // const selectorOutput = useRecoilValue(toDoSelector); // 아래와 같이 구조분해한다.
+  // console.log(selectorOutput);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
       <CreateToDo />
-      {toDos.map((toDo) => (
+      <h2>To Do</h2>
+      {toDo.map((toDo) => (
         <ToDo key={toDo.id} {...toDo} />
       ))}
+      <hr />
+      <h2>Doing</h2>
+      {doing.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
+      <hr />
+      <h3>Done</h3>
+      {done.map((toDo) => (
+        <ToDo key={toDo.id} {...toDo} />
+      ))}
+      <hr />
     </div>
   );
 };
--- todolist/src/atom.ts
+++ todolist/src/atom.ts
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
   text: string;
@@ -8,4 +8,17 @@ export interface IToDo {
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
   default: [],
+});
+
+// 아래 셀렉터는 toDoState에 뭉쳐있던 toDos를 분류해주고 있다.
+export const toDoSelector = selector({
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    return [
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((todo) => todo.category === "DOING"),
+      toDos.filter((todo) => todo.category === "DONE"),
+    ];
+  },
 });
